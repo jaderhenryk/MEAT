1d51c77e7ddcc535ffaeb11c35bc869d576a12ee
Jader Henrique de Paula <jaderhenryk@gmail.com>
2019-10-22 21:24:17 -0300

    creating first components and routes.
    creating header, home components and adding routes for them.

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mt-app',
+  templateUrl: './app.component.html'
+})
+export class AppComponent implements OnInit {
+  content = "Welcome to Meat App";
+  constructor () {};
+  ngOnInit() {};
+}
