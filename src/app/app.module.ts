a381a19820c3d944741c5382161ef67dc62d17c4
Jader Henrique de Paula <jaderhenryk@gmail.com>
2019-10-25 10:53:34 -0300

    Getting restaurants from json
    Getting restaurants data from json file.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
@@ -23,6 +24,7 @@ import { RestaurantsService } from './restaurants/restaurants.service';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     RouterModule.forRoot(ROUTES)
   ],
   providers: [RestaurantsService],
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,61 +1,18 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
 import {Restaurant} from './restaurant/restaurant.model';
 
+import {MEAT_API} from '../app.api'
+
+@Injectable()
 export class RestaurantsService {
-	// temporary data mock of restaurants
-	restaurants: Restaurant[] = [
-		{
-      "id": "bread-bakery",
-      "name": "Bread & Bakery",
-      "category": "Bakery",
-      "deliveryEstimate": "25m",
-      "rating": 4.9,
-      "imagePath": "assets/img/restaurants/breadbakery.png"
-    },
-    {
-      "id": "burger-house",
-      "name": "Burger House",
-      "category": "Hamburgers",
-      "deliveryEstimate": "100m",
-      "rating": 3.5,
-      "imagePath": "assets/img/restaurants/burgerhouse.png"
-    },
-    {
-      "id": "coffee-corner",
-      "name": "Coffee Corner",
-      "category": "Coffee Shop",
-      "deliveryEstimate": "30-40m",
-      "rating": 4.8,
-      "imagePath": "assets/img/restaurants/coffeecorner.png"
-    },
-    {
-      "id": "green-food",
-      "name": "Green Food",
-      "category": "Saudável",
-      "deliveryEstimate": "75m",
-      "rating": 4.1,
-      "imagePath": "assets/img/restaurants/greenfood.png"
-    },
-    {
-      "id": "ice-cream",
-      "name": "Ice Cream",
-      "category": "Ice Creams",
-      "deliveryEstimate": "40-65m",
-      "rating": 4.5,
-      "imagePath": "assets/img/restaurants/icy.png"
-    },
-    {
-      "id": "tasty-treats",
-      "name": "Tasty Treats",
-      "category": "Doces",
-      "deliveryEstimate": "20m",
-      "rating": 4.4,
-      "imagePath": "assets/img/restaurants/tasty.png"
-    }
-	];
+	
 
-	constructor () {};	
+	constructor (private httpClient: HttpClient) {};	
 
-	getRestaurants() : Restaurant[] {
-		return this.restaurants;
+	getRestaurants() : Observable<Restaurant[]> {
+		return this.httpClient.get(`${MEAT_API}/restaurants`);
 	}
 }
