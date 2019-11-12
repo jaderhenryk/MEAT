7605178ea708ee0debafb05cad4f729ef475a7a6
Jader Henrique de Paula <jaderhenryk@gmail.com>
2019-11-11 21:51:34 -0300

    Creating Modules to form components
    Creating a module for forms components like input component, radio component and rating component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,6 @@ import {ROUTES} from './app.routes';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
 import { RestaurantsService } from './restaurants/restaurants.service';
@@ -23,20 +22,17 @@ import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.compo
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
 import { OrderComponent } from './order/order.component';
-import { InputComponent } from './shared/input/input.component';
-import { RadioComponent } from './shared/radio/radio.component';
 import { OrdemItemComponent } from './order/ordem-item/ordem-item.component';
 import { OrderService } from './order/order.service';
 import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
-import { RatingComponent } from './shared/rating/rating.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
     declarations: [
         AppComponent,
         HeaderComponent,
         HomeComponent,
-        AboutComponent,
         RestaurantsComponent,
         RestaurantComponent,
         RestaurantDetailComponent,
@@ -45,18 +41,14 @@ import { RatingComponent } from './shared/rating/rating.component';
         MenuItemComponent,
         ReviewsComponent,
         OrderComponent,
-        InputComponent,
-        RadioComponent,
         OrdemItemComponent,
         DeliveryCostsComponent,
-        OrderSummaryComponent,
-        RatingComponent
+        OrderSummaryComponent
     ],
     imports: [
         BrowserModule,
         HttpClientModule,
-        FormsModule,
-        ReactiveFormsModule,
+        SharedModule,
         RouterModule.forRoot(ROUTES)
     ],
     providers: [
