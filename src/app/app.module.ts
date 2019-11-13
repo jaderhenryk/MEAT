91e11f60c0573e0642a1a23d495495c50e7e44b9
Jader Henrique de Paula <jaderhenryk@gmail.com>
2019-11-12 21:27:39 -0300

    Creating Order Module
    Creating order module for order and order items components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,10 +21,7 @@ import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shoppin
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
-import { OrderComponent } from './order/order.component';
-import { OrdemItemComponent } from './order/ordem-item/ordem-item.component';
 import { OrderService } from './order/order.service';
-import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { SharedModule } from './shared/shared.module';
 
@@ -40,9 +37,6 @@ import { SharedModule } from './shared/shared.module';
         ShoppingCartComponent,
         MenuItemComponent,
         ReviewsComponent,
-        OrderComponent,
-        OrdemItemComponent,
-        DeliveryCostsComponent,
         OrderSummaryComponent
     ],
     imports: [
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,6 @@ import {RestaurantsComponent} from './restaurants/restaurants.component';
 import {RestaurantDetailComponent} from './restaurant-detail/restaurant-detail.component';
 import {MenuComponent} from './restaurant-detail/menu/menu.component';
 import {ReviewsComponent} from './restaurant-detail/reviews/reviews.component';
-import {OrderComponent} from './order/order.component';
 import {OrderSummaryComponent} from './order-summary/order-summary.component';
 
 export const ROUTES = [
@@ -19,6 +18,6 @@ export const ROUTES = [
 			{path: 'reviews', component: ReviewsComponent}
 		]	
 	},
-	{path: 'order', component: OrderComponent},
+	{path: 'order', loadChildren: './order/order.module#OrderModule'},
 	{path: 'order-summary', component: OrderSummaryComponent}
 ]
