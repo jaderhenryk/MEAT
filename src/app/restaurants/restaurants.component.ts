4b56c19ee29e83846639ec404a2b0dd09b05ff3e
Jader Henrique de Paula <jaderhenryk@gmail.com>
2019-11-26 21:06:03 -0300

    Implementing restaurant search
    Implementing search for restaurants.

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,22 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 import { Restaurant } from './restaurant/restaurant.model';
 import { RestaurantsService } from './restaurants.service';
 import { ErrorHandler } from '../app.error-handler';
 
+import { Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+
 @Component({
     selector: 'mt-restaurants',
-    templateUrl: './restaurants.component.html'
+    templateUrl: './restaurants.component.html',
+    animations: [
+        trigger('toggleSearch', [
+            state('hidden', style({
+                opacity: 0,
+                'max-height': '0px'
+            })),
+            state('visible', style({
+                opacity: 1,
+                'max-height': '70px',
+                'margin-top': '20px'
+            })),
+            transition('* => *', animate('250ms 0s ease-in-out'))
+        ])
+    ]
 })
 export class RestaurantsComponent implements OnInit {
 
+    searchBarState = 'hidden';
+    searchControl: FormControl;
+    searchForm: FormGroup;
+
     restaurants: Restaurant[];
 
-    constructor(private restaurantService: RestaurantsService) { };
+    constructor(private restaurantService: RestaurantsService, private formBuilder: FormBuilder) { };
 
     ngOnInit() {
+        this.searchControl = this.formBuilder.control('');
+        this.searchForm = this.formBuilder.group({
+            searchControl: this.searchControl
+        });
+        this.searchControl.valueChanges.pipe(
+            debounceTime(500), 
+            distinctUntilChanged(), 
+            switchMap(searchTerm => this.restaurantService.restaurants(searchTerm))
+        ).subscribe(restaurants => this.restaurants = restaurants, err => console.log(ErrorHandler.handleError(err)));
         this.restaurantService.restaurants()
             .subscribe(restaurants => this.restaurants = restaurants, err => console.log(ErrorHandler.handleError(err)));
+    }
+
+    toggleSearchBar() {
+        this.searchBarState = this.searchBarState === 'hidden' ? 'visible' : 'hidden';
     }
 
 }
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,30 +1,30 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-import {Restaurant} from './restaurant/restaurant.model';
-import {MenuItem} from '../restaurant-detail/menu-item/menu-item.model';
+import { Restaurant } from './restaurant/restaurant.model';
+import { MenuItem } from '../restaurant-detail/menu-item/menu-item.model';
 
-import {MEAT_API} from '../app.api'
+import { MEAT_API } from '../app.api'
 
 @Injectable()
 export class RestaurantsService {
-	
-	constructor (private httpClient: HttpClient) {};	
 
-	restaurants() : Observable<Restaurant[]> {
-		return this.httpClient.get<Restaurant[]>(`${MEAT_API}/restaurants`);
-	}
+    constructor(private httpClient: HttpClient) { };
 
-	restaurant(id: string) : Observable<Restaurant> {
-		return this.httpClient.get<Restaurant>(`${MEAT_API}/restaurants/${id}`);
-	}
+    restaurants(search: string = ''): Observable<Restaurant[]> {
+        return this.httpClient.get<Restaurant[]>(`${MEAT_API}/restaurants`, {params: {q: search}});
+    }
 
-	reviews(id: string) : Observable<any> {
-		return this.httpClient.get<any>(`${MEAT_API}/restaurants/${id}/reviews`);
-	}
+    restaurant(id: string): Observable<Restaurant> {
+        return this.httpClient.get<Restaurant>(`${MEAT_API}/restaurants/${id}`);
+    }
 
-	menu(id: string) : Observable<MenuItem[]> {
-		return this.httpClient.get<MenuItem[]>(`${MEAT_API}/restaurants/${id}/menu`);
-	}
+    reviews(id: string): Observable<any> {
+        return this.httpClient.get<any>(`${MEAT_API}/restaurants/${id}/reviews`);
+    }
+
+    menu(id: string): Observable<MenuItem[]> {
+        return this.httpClient.get<MenuItem[]>(`${MEAT_API}/restaurants/${id}/menu`);
+    }
 }
