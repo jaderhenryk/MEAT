f04ce4b175429d8273c1755529757bc8de83d7aa
Jader Henrique de Paula <jaderhenryk@gmail.com>
2019-12-19 21:38:51 -0300

    Creating login form
    Creating login form component and adding some validations.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,9 +7,12 @@ import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { LoginComponent } from './security/login/login.component';
+
 
 export const ROUTES = [
     { path: '', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
     { path: 'about', loadChildren: './about/about.module#AboutModule' },
     { path: 'restaurants', component: RestaurantsComponent },
     {
