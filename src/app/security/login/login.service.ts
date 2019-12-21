b896204a37faa42bb4f185577197593e9139b861
Jader Henrique de Paula <jaderhenryk@gmail.com>
2019-12-20 21:28:21 -0300

    Implementing new features in login
    Adding sign in/out item on menu;
    Redirect user after login to previous accessed page.

--- src/app/security/login/login.service.ts
+++ src/app/security/login/login.service.ts
@@ -3,25 +3,32 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { MEAT_API } from '../../app.api';
 import { User } from '../user/user.model';
-import { tap } from 'rxjs/operators';
-import { Router } from '@angular/router';
+import { tap, filter } from 'rxjs/operators';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Injectable()
 export class LoginService {
 
     user: User;
+    previousUrl: string;
 
-    constructor(private httpClient: HttpClient, private router: Router) {}
+    constructor(private httpClient: HttpClient, private router: Router) {
+        this.router.events.pipe(filter(e => e instanceof NavigationEnd)).subscribe((e: NavigationEnd) => this.previousUrl = e.url);
+    }
 
     login(email: string, password: string): Observable<User> {
         return this.httpClient.post<User>(`${MEAT_API}/login`, {email, password}).pipe(tap(user => this.user = user));
+    }
+
+    logout() {
+        this.user = undefined;
     }
 
     isLoggedIn(): boolean {
         return this.user !== undefined;
     }
 
-    handleLogin(path?: string) {
+    handleLogin(path: string = this.previousUrl) {
         this.router.navigate(['/login', btoa(path)]);
     }
 }
